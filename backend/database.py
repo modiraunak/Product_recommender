4e0ac0c0560b07221936519037a40bb95d65c36f
Raunak Kumar Modi <raunakk046@gmail.com>
2025-05-22 18:58:49 +0530

    updated database.py

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def init_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///orders.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     db.init_app(app)
     with app.app_context():
         db.create_all()
